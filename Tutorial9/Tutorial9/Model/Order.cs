6ae261bdd7d21bc7999dc5cfa9e330555fdb26cb
Dana Nazarchuk <s29888@pjwstk.edu.pl>
2025-05-05 11:05:33 +0200

    del order

---
+++ Tutorial9/Tutorial9/Model/Order.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Tutorial9.Model;
+
+public class Order
+{
+    public int IdOrder { get; set; }
+    public int IdProduct { get; set; }
+    public int Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    
+}
